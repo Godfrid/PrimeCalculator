a253e29119419bd318059f3ad4b26e233e7fc2a1
Godfrid <godfrid@gmail.com>
2018-09-12 21:01:29 +0200

    AKS test imported.

---
+++ Main/src/logic/GCD/GreatestCommonDivisor.java
@@ -0,0 +1,24 @@
+package logic.GCD;
+
+import java.math.BigInteger;
+
+public  class  GreatestCommonDivisor {
+
+    public static long GCD(long a, long b) {
+        long t;
+        while(b != 0) {
+            t = b;
+            b = a % b;
+            a = t;
+        }
+        return a;
+    }
+
+    public static BigInteger GCD(BigInteger number1, BigInteger number2) {
+        return number1.gcd(number2);
+    }
+
+
+
+
+}
