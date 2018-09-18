d9b5038598270d42180c11cb03e55d05969b08dc
Godfrid <godfrid@gmail.com>
2018-09-18 16:27:37 +0200

    UI init

--- Main/src/ui/Ui.java
+++ Main/src/ui/Ui.java
@@ -3,50 +3,17 @@ package ui;
 import javax.swing.*;
 import java.awt.*;
 
-public class Ui extends JFrame {
+public class Ui {
+    JFrame primeTesterUI;
+    JPanel mainPanel;
+    private JButton startButton;
+    private JTextField inputTextField;
+    private JComboBox engineSelector;
+    private JLabel TestType;
+    private JLabel inputLabel;
+    private JLabel isPrime;
+    private JProgressBar progressBar;
+    private JComboBox coreSelector;
 
-    private JLabel resultBar;
-    private JPanel root;
-    private Dimension preferedSize = new Dimension(800, 600);
-    private Dimension maxSize = new Dimension(1240, 900);
-    private Dimension minSize = new Dimension(648, 480);
-
-    public Ui() {
-
-        setTitle("Prime tester");
-        setPreferredSize(preferedSize);
-        setMaximumSize(maxSize);
-        setMinimumSize(minSize);
-        setSize(preferedSize);
-        setDefaultCloseOperation(EXIT_ON_CLOSE);
-        setResizable(true);
-        setLocationRelativeTo(null);
-
-
-        root = new JPanel();
-        root.setLayout(null);
-        root.setBackground(Color.BLACK);
-        root.setOpaque(true);
-        this.add(root);
-
-        resultBar = new JLabel();
-        resultBar.setText("kek");
-        resultBar.setBounds(0, 440, 250, 20);
-        resultBar.setBackground(Color.RED);
-        resultBar.setOpaque(true);
-        root.add(resultBar);
-
-        setVisible(true);
-    }
-
-    private void resultBar() {
-        JLabel resultBar = new JLabel();
-        resultBar.setText("kek");
-        resultBar.setLocation(480, 10);
-        resultBar.setSize(50, 10);
-        resultBar.setBackground(Color.RED);
-        resultBar.setOpaque(true);
-        this.add(resultBar);
-    }
 
 }
